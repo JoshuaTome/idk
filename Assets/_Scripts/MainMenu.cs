05be1caa890249f88083972bdc97e7a760f4b659
Josh <joshuatome2000@gmail.com>
2021-02-16 14:59:46 -0500

    added features

---
+++ Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    void Update()
+    {
+        
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
